3cca10d4909132027922bebdfd2a0b9a3fd49300
SriHarsh27 <harshkashyap.27@gmail.com>
2021-01-07 19:08:30 +0530

    lesson-04

--- app/src/main/java/com/example/swoosh/LayoutActivity.kt
+++ app/src/main/java/com/example/swoosh/LayoutActivity.kt
@@ -1,11 +1,17 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LayoutActivity : AppCompatActivity() {
+class LayoutActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout)
+    }
+    fun leagueNextClicked(view: View){
+        val skillActivity = Intent(this,SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
